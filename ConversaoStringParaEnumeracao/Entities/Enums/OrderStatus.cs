b7faa4bcfef860c5671bc40dfd9f083986d3957e
Adriano Lima <adriano.lim85@yahoo.com.br>
2020-06-15 15:25:30 -0300

    Exemplos de tipo enumerado

---
+++ ConversaoStringParaEnumeracao/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ConversaoStringParaEnumeracao.Entities.Enums
+{
+    enum OrderStatus : int // Especificando q cada valor sera INT
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ ConversaoStringParaEnumeracao/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConversaoStringParaEnumeracao.Entities;
+using ConversaoStringParaEnumeracao.Entities.Enums;
+using System;
+
+namespace ConversaoStringParaEnumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Instanciando um pedido:
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+            Console.WriteLine();
+
+
+            // Conversao de enumerado para String:
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            // Convertendo de string para enumerado:
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+
+        }
+    }
+}
